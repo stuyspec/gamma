bc97a8755d87bb8b4e25fa8ac2c79bac0d9425a4
Bi1ku <2008owenshi@gmail.com>
2025-03-08 15:05:04 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-green-700">GAMMA</div>;
+}
+
+export default App;
---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<App />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
